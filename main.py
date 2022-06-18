b9022f98661a4c558435904e71165284637ce8cf
Devin Yang <70051464+hedgehog-qd@users.noreply.github.com>
2022-06-18 22:35:27 +0800

    Add the production environment
    Add the production environment

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template
 import api_dealing
 import requests
 import json_dealing
+from gevent import pywsgi #added this for production environment
 app = Flask(__name__)
 
 
@@ -45,5 +46,8 @@ def index():
     return render_template('index.html', p1='done', p2=result)
 
 if "__main__"==__name__:
-    app.run(port="5008")
+    #app.run(port="5008")  #this is only for test environment on the localhost. Use the following as the production environment.
+    
+    server = pywsgi.WSGIServer(('0.0.0.0',5008),app)  #The server would be running at 5008 port
+    server.serve_forever()  #Service start
 
