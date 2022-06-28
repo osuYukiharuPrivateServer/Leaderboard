c0cb70b72952e5fefedd28c26e0816bc8af31bb1
Devin Yang <70051464+hedgehog-qd@users.noreply.github.com>
2022-06-28 13:04:37 +0800

    Update json_dealing.py

--- json_dealing.py
+++ json_dealing.py
@@ -3,8 +3,8 @@ import json
 
 
 
-def DealJson():
-    jsonn = api_dealing.request_data()
+def DealJson(mode,sort):
+    jsonn = api_dealing.request_data(mode,sort)
     json_str = json.dumps(jsonn)
     data2 = json.loads(json_str)
     sta = data2['status']
