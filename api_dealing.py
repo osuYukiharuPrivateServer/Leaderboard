c4fa6105375648c1e821bdeff42cc019bbda3f6c
Devin Yang <70051464+hedgehog-qd@users.noreply.github.com>
2022-06-28 13:04:54 +0800

    Update api_dealing.py

--- api_dealing.py
+++ api_dealing.py
@@ -1,7 +1,9 @@
 import requests
-url = "https://api.yukiharu.pw/get_leaderboard"
 
-def request_data():
+
+def request_data(mode,sort):
+    url = "https://api.yukiharu.pw/get_leaderboard"
+    url = url+"?mode="+mode+"&sort="+sort
     req = requests.get(url, timeout=30)  # 请求连接
     req_json = req.json()  # 获取数据
     return req_json
